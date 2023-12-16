9accf2c7536761c84fbc390bc28da356e0009fcf
Josué Costa <josuecosta077@gmail.com>
2023-12-16 12:45:29 -0300

    Continuando aulas de typescript

---
+++ Aulas Typescript/aula04-tipos-tuplas/index.ts
@@ -0,0 +1,12 @@
+let fruits: string[] = [] //array de que só recebe strings, sem limite
+
+fruits.push('banana')
+fruits.push(1) 
+
+let fruits2: [string, string, string] = [] //tupla, é tipo um array com tamanho maximo definido
+
+fruits2[0] = 'maca'
+fruits2[0] = 2
+fruits2[1] = 'banana'
+fruits2[2] = 'pera'
+fruits2[3] = 'melancia'
---
+++ Aulas Typescript/aula05-tipos-enums/index.ts
@@ -0,0 +1,16 @@
+enum planetas { //definição de valores para certas palavras
+    MERCURIO = 'mercurio',
+    VENUS = 2,
+    TERRA = 'terra',
+    MARTE = 3
+}
+
+planetas.MERCURIO
+planetas.VENUS
+planetas.TERRA
+planetas.MARTE
+
+enum Roles { //aplicação real
+    ADMIN = 'admin',
+    USER = 'user'
+}
