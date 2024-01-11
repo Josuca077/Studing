b9b582f2db5da4a3e8cf2f6b790fedb146f46829
Josué Costa <josuecosta077@gmail.com>
2024-01-11 12:28:17 -0300

    finished exercise 26

--- Exercícios/exercicio26-criando-Tipos/public/dist/index.js
+++ Exercícios/exercicio26-criando-Tipos/public/dist/index.js
@@ -18,31 +18,19 @@ function registerPlanet(name, coordinates, situation) {
     };
     planets.push(planet);
 }
-function updateSituation(name, situation) {
-    try {
-        const planet = findPlanet(name);
-        planet.situation = situation;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
+function updateSituation(planet, situation) {
+    planet.situation = situation;
 }
-function addSatellite(name, satellite) {
-    try {
-        const planet = findPlanet(name);
-        planet.satellites.push(satellite);
-    }
-    catch (error) {
-        console.log(error.message);
-    }
+function addSatellite(planet, satellite) {
+    planet.satellites.push(satellite);
 }
-function removeSatellite(name, satellite) {
-    try {
-        const planet = findPlanet(name);
+function removeSatellite(planet, satellite) {
+    if (planet.satellites.includes(satellite)) {
         planet.satellites = planet.satellites.filter((item) => item !== satellite);
     }
-    catch (error) {
-        console.log(error.message);
+    else {
+        alert(`O satélite ${satellite} não está registrado no Planeta ${planet.name}`);
+        alert("Retornando para o menu...");
     }
 }
 function listPlanets() {
@@ -50,20 +38,45 @@ function listPlanets() {
         return (`${mensage}
 ${planet.name}:
 
-Coordenadas: ${planet.coordinates}
+Coordenadas: ${planet.coordinates[0]} ${planet.coordinates[1]} ${planet.coordinates[2]} ${planet.coordinates[3]}
 Situação: ${planet.situation}
-Satélites: ${planet.satellites}
+Satélites: ${planet.satellites.reduce((list, satellite) => {
+            return (`${list}${satellite}  `);
+        }, "")}
         
 `);
     }, ""));
+}
+function chooseSituation() {
+    let choice;
+    do {
+        choice = parseFloat(prompt(`Escolha a situação do planeta:
+
+1- Habitado
+2- Habitável
+3- Inabitável
+4- Inexplorado
+`));
+        switch (choice) {
+            case 1:
+                return "habitado";
+            case 2:
+                return "habitável";
+            case 3:
+                return "inabitável";
+            case 4:
+                return "inexplorado";
+        }
+    } while (choice !== 1 && choice !== 2 && choice !== 3 && choice !== 4);
 }
 function openMenu() {
     const choice = parseFloat(prompt(`Escolha a Ação:
 
 1- Registrar Planeta
 2- Atualizar Situação
-3- Alterar Satélites
-4- Listar Planetas
+3- Adicionar Satélite
+4- Remover Satélite
+5- Listar Planetas
 
 0- SAIR`));
     switch (choice) {
@@ -79,6 +92,9 @@ function openMenu() {
         case 4:
             choice4();
             break;
+        case 5:
+            choice5();
+            break;
         case 0:
             exit();
             break;
@@ -89,54 +105,65 @@ function openMenu() {
 }
 function choice1() {
     const name = prompt("Digite o nome do planeta a ser registrado:");
+    let coordinates1;
+    let coordinates2;
+    let coordinates3;
+    let coordinates4;
+    let confirmation;
     try {
         findPlanet(name);
     }
     catch {
-        const coordinates1 = parseFloat(prompt("Digite o primeiro das coordenadas:"));
-        const coordinates2 = parseFloat(prompt("Digite o segundo das coordenadas:"));
-        const coordinates3 = parseFloat(prompt("Digite o terceiro das coordenadas:"));
-        const coordinates4 = parseFloat(prompt("Digite o quarto das coordenadas:"));
-        let situation;
-        let choice;
         do {
-            choice = parseFloat(prompt(`Escolha a situação do planeta:
-
-1- Habitado
-2- Habitável
-3- Inabitável
-4- Inexplorado
-`));
-            switch (choice) {
-                case 1:
-                    situation = "habitado";
-                    break;
-                case 2:
-                    situation = "habitável";
-                    break;
-                case 3:
-                    situation = "inabitável";
-                    break;
-                case 4:
-                    situation = "inexplorado";
-                    break;
+            coordinates1 = parseFloat(prompt("Digite o primeiro das coordenadas:"));
+            coordinates2 = parseFloat(prompt("Digite o segundo das coordenadas:"));
+            coordinates3 = parseFloat(prompt("Digite o terceiro das coordenadas:"));
+            coordinates4 = parseFloat(prompt("Digite o quarto das coordenadas:"));
+            confirmation = true;
+            if (isNaN(coordinates1) || isNaN(coordinates2) || isNaN(coordinates3) || isNaN(coordinates4)) {
+                alert("Todas as coordenadas devem ser números!");
+                confirmation = false;
             }
-        } while (choice !== 1 && choice !== 2 && choice !== 3 && choice !== 4);
+        } while (!confirmation);
+        const situation = chooseSituation();
         registerPlanet(name, [coordinates1, coordinates2, coordinates3, coordinates4], situation);
     }
     openMenu();
 }
 function choice2() {
     const name = prompt("Escreva o nome do planeta que deseja alterar a situação:");
+    try {
+        updateSituation(findPlanet(name), chooseSituation());
+    }
+    catch (error) {
+        alert(error.message);
+        alert("Voltando para o Menu...");
+    }
     openMenu();
 }
 function choice3() {
-}
-function choice3A() {
-}
-function choice3B() {
+    const name = prompt("Escreva o nome do planeta que deseja adicionar um satélite:");
+    try {
+        addSatellite(findPlanet(name), prompt(`Escreva o nome do Satélite de ${name}:`));
+    }
+    catch (error) {
+        alert(error.message);
+        alert("Voltando para o Menu...");
+    }
+    openMenu();
 }
 function choice4() {
+    const name = prompt("Escreva o nome do planeta que deseja remover um satélite:");
+    try {
+        removeSatellite(findPlanet(name), prompt(`Escreva o nome do Satélite que deseja remover de ${name}:`));
+    }
+    catch (error) {
+        alert(error.message);
+        alert("Voltando para o Menu...");
+    }
+    openMenu();
+}
+function choice5() {
     listPlanets();
     openMenu();
 }
--- Exercícios/exercicio26-criando-Tipos/src/index.ts
+++ Exercícios/exercicio26-criando-Tipos/src/index.ts
@@ -1,5 +1,13 @@
-const planets = []
 type status = "habitado" | "habitável" | "inabitável" | "inexplorado"
+type coordinates = [number, number, number, number]
+type planet = {
+    name: string,
+    coordinates: coordinates,
+    situation: status,
+    satellites: string[]
+}
+const planets: planet[] = []
+
 document.getElementById('btn').addEventListener('click', openMenu)
 
 function findPlanet (name: string){
@@ -18,7 +26,7 @@ function findPlanet (name: string){
 
 } 
 
-function registerPlanet (name: string, coordinates: [number, number, number, number], situation: status) {
+function registerPlanet (name: string, coordinates: coordinates, situation: status) {
 
     const planet = {
         name,
@@ -30,68 +38,86 @@ function registerPlanet (name: string, coordinates: [number, number, number, num
     planets.push(planet)
 }
 
-function updateSituation(name: string, situation: status) {
+function updateSituation(planet: planet, situation: status) {
 
-    try{
-
-        const planet = findPlanet(name)
-        planet.situation = situation
-        
-    } catch (error) {
-
-        console.log(error.message)
-
-    }
+    planet.situation = situation
 
 }
 
-function addSatellite(name:string, satellite: string) {
+function addSatellite(planet: planet, satellite: string) {
 
-    try{
-
-        const planet = findPlanet(name)
-        planet.satellites.push(satellite)
-        
-    } catch (error) {
-
-        console.log(error.message)
-
-    }
+    planet.satellites.push(satellite)
 
 }
 
-function removeSatellite(name:string, satellite: string) {
+function removeSatellite(planet: planet, satellite: string) {
 
-    try{
+    if (planet.satellites.includes(satellite)) {
 
-        const planet = findPlanet(name)
         planet.satellites = planet.satellites.filter((item: string) => item !== satellite)
-        
-    } catch (error) {
 
-        console.log(error.message)
+    } else {
+
+        alert(`O satélite ${satellite} não está registrado no Planeta ${planet.name}`)
+        alert("Retornando para o menu...")
 
     }
+        
 
 }
 
 function listPlanets () {
 
-    alert(planets.reduce((mensage, planet) => {
+    alert(planets.reduce((mensage: string, planet: {name: string, coordinates: [number, number, number, number], situation: status, satellites: string[]}) => {
 
         return (
 `${mensage}
 ${planet.name}:
 
-Coordenadas: ${planet.coordinates}
+Coordenadas: ${planet.coordinates[0]} ${planet.coordinates[1]} ${planet.coordinates[2]} ${planet.coordinates[3]}
 Situação: ${planet.situation}
-Satélites: ${planet.satellites}
+Satélites: ${planet.satellites.reduce((list: string, satellite: string) => {
+
+    return (
+`${list}${satellite}  `        
+    )
+
+}, "")}
         
 `
 )
 
     }, ""))
 
+
+}
+
+function chooseSituation () {
+
+        let choice: number
+
+        do {
+            choice = parseFloat(prompt(
+`Escolha a situação do planeta:
+
+1- Habitado
+2- Habitável
+3- Inabitável
+4- Inexplorado
+`
+))
+
+            switch (choice) {
+                case 1:
+                    return "habitado"
+                case 2:
+                    return "habitável"
+                case 3:
+                    return "inabitável"
+                case 4:
+                    return "inexplorado"
+                }
+        } while (choice !== 1 && choice !== 2 && choice !== 3 && choice !== 4)
 
 }
 
@@ -103,8 +129,9 @@ function openMenu () {
 
 1- Registrar Planeta
 2- Atualizar Situação
-3- Alterar Satélites
-4- Listar Planetas
+3- Adicionar Satélite
+4- Remover Satélite
+5- Listar Planetas
 
 0- SAIR`
 
@@ -123,6 +150,9 @@ function openMenu () {
         case 4:
             choice4()
             break
+        case 5:
+            choice5()
+            break
         case 0:
             exit()
             break
@@ -136,47 +166,34 @@ function openMenu () {
 function choice1 () {
 
     const name = prompt("Digite o nome do planeta a ser registrado:")
-    
+    let coordinates1: number
+    let coordinates2: number
+    let coordinates3: number
+    let coordinates4: number
+    let confirmation: boolean
+
     try {
         findPlanet(name)
     } catch {
 
-        const coordinates1 = parseFloat(prompt("Digite o primeiro das coordenadas:"))
-        const coordinates2 = parseFloat(prompt("Digite o segundo das coordenadas:"))
-        const coordinates3 = parseFloat(prompt("Digite o terceiro das coordenadas:"))
-        const coordinates4 = parseFloat(prompt("Digite o quarto das coordenadas:"))
+        do{
 
-        let situation: status
+            coordinates1 = parseFloat(prompt("Digite o primeiro das coordenadas:"))
+            coordinates2 = parseFloat(prompt("Digite o segundo das coordenadas:"))
+            coordinates3 = parseFloat(prompt("Digite o terceiro das coordenadas:"))
+            coordinates4 = parseFloat(prompt("Digite o quarto das coordenadas:"))
 
-        let choice: 1 | 2 | 3 | 4
+            confirmation = true
 
-        do {
-            choice = parseFloat(prompt(
-`Escolha a situação do planeta:
+            if (isNaN(coordinates1) || isNaN(coordinates2) || isNaN(coordinates3) || isNaN(coordinates4)) {
+                alert("Todas as coordenadas devem ser números!")
+                confirmation = false
+            }
 
-1- Habitado
-2- Habitável
-3- Inabitável
-4- Inexplorado
-`
-))
+        } while (!confirmation)
 
-            switch (choice) {
-                case 1:
-                    situation = "habitado"
-                    break
-                case 2:
-                    situation = "habitável"
-                    break
-                case 3:
-                    situation = "inabitável"
-                    break
-                case 4:
-                    situation = "inexplorado"
-                    break
-                }
-        } while (choice !== 1 && choice !== 2 && choice !== 3 && choice !== 4)
-        
+
+        const situation = chooseSituation()
 
         registerPlanet(name, [coordinates1, coordinates2, coordinates3, coordinates4], situation)
     }
@@ -189,6 +206,17 @@ function choice2 () {
 
     const name = prompt("Escreva o nome do planeta que deseja alterar a situação:")
 
+    try {
+
+        updateSituation(findPlanet(name), chooseSituation())
+
+    } catch (error) {
+
+        alert(error.message)
+        alert("Voltando para o Menu...")
+    }
+
+    
 
     openMenu()
 
@@ -196,17 +224,47 @@ function choice2 () {
 
 function choice3 () {
 
-}
+    const name = prompt("Escreva o nome do planeta que deseja adicionar um satélite:")
 
-function choice3A () {
+    try {
 
-}
+        addSatellite(findPlanet(name), prompt(`Escreva o nome do Satélite de ${name}:`))
 
-function choice3B () {
+    } catch (error) {
 
+        alert(error.message)
+        alert("Voltando para o Menu...")
+
+    }
+
+    
+
+    openMenu()
+
+    
 }
 
 function choice4 () {
+
+    const name = prompt("Escreva o nome do planeta que deseja remover um satélite:")
+
+    try {
+
+        removeSatellite(findPlanet(name), prompt(`Escreva o nome do Satélite que deseja remover de ${name}:`))
+
+    } catch (error) {
+
+        alert(error.message)
+        alert("Voltando para o Menu...")
+
+    }
+
+    
+    openMenu()
+
+}
+
+function choice5 () {
 
     listPlanets()
     openMenu()
