d4b0f1d7bbd5311482036ef29420e96e6a2aefb9
Josué Costa <josuecosta077@gmail.com>
2024-01-18 01:43:23 -0300

    starting reactDOM

---
+++ Aulas ReactDOM/aula02-conhecendo-JSX/index.jsx
@@ -0,0 +1,16 @@
+const title = <h1>Olá mundo!</h1>
+
+const element = 
+
+<div>
+    <h1></h1>
+
+    <ul></ul>
+    <ul></ul>
+    <ul></ul>
+    <ul></ul>
+    <ul></ul>
+    <ul></ul>
+
+    <h2></h2>
+</div>
---
+++ Aulas ReactDOM/aula03-components-React/index.jsx
@@ -0,0 +1,16 @@
+
+function Button() {
+    return <button>Meu botão</button>
+}
+
+function Paragraph () {
+    return (
+        <div>
+            <article id="artigo" className="texto">
+                <p>1</p>
+                <p>2</p>
+            </article>
+            <label htmlFor="a"></label>
+        </div>
+            )
+}
